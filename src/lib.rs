cf79d211c1f8d4a0972c85e7af0a81c6327dd791
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2023-10-01 10:30:45 +0300

    Handle pdfs

--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,17 @@ use std::fs;
 use std::io::Error;
 use std::path::{Path, PathBuf};
 
-pub fn initialize_directories(desktop_dir: &Path) -> (PathBuf, PathBuf) {
+pub fn initialize_directories(desktop_dir: &Path) -> (PathBuf, PathBuf, PathBuf) {
     // Create destination directories, if they're missing.
     let screenshors_dir = desktop_dir.join("gnome-screenshots");
     let images_dir = desktop_dir.join("gnome-images");
+    let pdfs_dir = desktop_dir.join("gnome-pdfs");
 
     fs::create_dir_all(&screenshors_dir).unwrap();
     fs::create_dir_all(&images_dir).unwrap();
+    fs::create_dir_all(&pdfs_dir).unwrap();
 
-    (images_dir, screenshors_dir)
+    (images_dir, screenshors_dir, pdfs_dir)
 }
 
 pub fn is_image_file(file_path: &Path) -> bool {
@@ -33,6 +35,14 @@ pub fn is_screenshot(file_path: &Path) -> bool {
     }
 }
 
+pub fn is_pdf_file(file_path: &Path) -> bool {
+    if let Some(ext) = file_path.extension() {
+        matches!(ext.to_str(), Some("pdf"))
+    } else {
+        false
+    }
+}
+
 pub fn get_desktop_dir() -> Option<PathBuf> {
     dirs::desktop_dir()
 }
@@ -40,7 +50,6 @@ pub fn get_desktop_dir() -> Option<PathBuf> {
 pub fn move_file(file_path: &Path, dest_dir: &Path) -> Result<(), Error> {
     // Gracefully moves files without overwrites or samename losses.
     let mut new_path = dest_dir.join(file_path.file_name().unwrap());
-    print!("{:?}", new_path);
 
     let mut counter = 1;
     while new_path.exists() {
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,40 @@
 use std::fs;
 use std::io;
-use gnome::{initialize_directories, get_desktop_dir, is_image_file, is_screenshot, move_file};
+use gnome::{initialize_directories, get_desktop_dir, is_image_file, is_screenshot, is_pdf_file, move_file};
 
 fn main() -> io::Result<()> {
     if let Some(desktop_dir) = get_desktop_dir() {
-        let (images_dir, screenshots_dir) = initialize_directories(&desktop_dir);
+        let (images_dir, screenshots_dir, pdfs_dir) = initialize_directories(&desktop_dir);
         let mut images_count = 0;
         let mut screenshots_count = 0;
+        let mut pdfs_count = 0;
 
         for entry in fs::read_dir(&desktop_dir)? {
             let entry = entry?;
             let file_path = entry.path();
 
-            if file_path.is_file() && is_image_file(&file_path) {
-                let mut dest_dir = &images_dir;
-                images_count += 1;
+            if file_path.is_file() {
+                if is_image_file(&file_path) {
+                    let mut dest_dir = &images_dir;
+                    images_count += 1;
 
-                if is_screenshot(&file_path) {
-                    dest_dir = &screenshots_dir;
-                    screenshots_count += 1;
-                    images_count -= 1;
+                    if is_screenshot(&file_path) {
+                        dest_dir = &screenshots_dir;
+                        screenshots_count += 1;
+                        images_count -= 1;
+                    }
+
+                    move_file(&file_path, dest_dir)?;
                 }
 
-                move_file(&file_path, dest_dir)?;
+                if is_pdf_file(&file_path) {
+                    pdfs_count += 1;
+                    move_file(&file_path, &pdfs_dir)?;
+                }
             }
         }
 
-        println!("Gnome has finished cleaning up the desktop. Moved {} images and {} screenshots", images_count, screenshots_count);
+        println!("Gnome has finished cleaning up the desktop. Moved {} images, {} screenshots and {} pdfs.", images_count, screenshots_count, pdfs_count);
     } else {
         println!("Gnome could not find the desktop :(.");
     }
